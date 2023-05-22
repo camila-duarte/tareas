78d6a211f7835cd9debf5bf96e342dc0c3ffdb3e
cmdm2022 <camiladuartemaidana@gmail.com>
2023-05-22 11:26:58 -0400

    update

--- rungekutta.py
+++ rungekutta.py
@@ -1,49 +1,54 @@
-from scipy.integrate import odeint
-import math  # Usar en caso te tener alguna funcion trigonometrica
+import math
 
-
+# > h) d^2y/dt^2 - 4dy/dt + 4y = e^(-t), y(0) = 0, dy/dt(0) = 1  
 # Introducir la ecuacion
-def f(t, y):
-    return -2*t*y
+
+
+def primera_derivada(t, y):
+    return -y + math.e**(-t)
+
+def segunda_derivada(t, y, z): 
+    return  4*primera_derivada(t, y) - 4*y + math.e**(-t)
+
+4*primera_derivada(t,y) - 4x+ e
 
 
 # Introducir los valores iniciales
 def introducir_variables():
-    global tk, dt, yk, n, t, y, y_inicial
+    global tk, dt, y0, n, t, y, z0
+    # tk <- Es el tiempo inicial que luego va cambiando con dt
     tk = float(input("Ingrese el tiempo inicial: "))
+    # dt <- Es el tamanho de los pasos del tiempo (delta t) del
+    # cual depende la presicion del resultado
     dt = float(input("Ingrese la dt: "))
-    yk = float(input("Ingrese la condicion inicial: "))
+    # Es el valor de y en el tiempo inicial
+    y0 = float(input("Ingrese la condicion inicial: "))
+    z0 = float(input("Ingrese la condicion inicial de la primera derivada: "))
+    # Es la cantidad de resultados de y que se quiere
     n = int(input("Ingrese la cantidad de pasos: "))
     t = 0
     y = 0
-    y_inicial = yk
-    return tk, dt, yk, n, t, y, y_inicial
+    return tk, dt, y0, n, t, y, z0
 
+z = primera_derivada(tk, y0)
 
 introducir_variables()
-
-
-f(t, y)
 
 # Siendo h el iterador que va a contar los pasos y
 # siendo n la cantidad de pasos que debe realizar
 for h in range(n):
-    k1 = dt * f(tk, yk)
+    k1 = dt * z(tk, y0, z0)
 
-    k2 = dt * f(tk + dt/2, yk + k1/2)
+    k2 = dt * f(tk + dt/2, y0 + k1/2, )
 
-    k3 = dt * f(tk + dt/2, yk + k2/2)
+    k3 = dt * f(tk + dt/2, y0 + k2/2)
 
-    k4 = dt * f(tk + dt, yk + k3)
+    k4 = dt * f(tk + dt, y0 + k3)
 
-    y_resultado = yk + ((k1 + (2 * k2) + (2 * k3) + k4) / 6)
+    yk = y0 + ((k1 + (2 * k2) + (2 * k3) + k4) / 6)
 
-    yk = y_resultado
+    y0 = yk
 
     tk = tk + dt
 
-    print(y_resultado)
-
-# Para comparar los resultados
-resultado = odeint(f, yk, [tk, n])[-1]
-print(resultado)
+    print(yk)
